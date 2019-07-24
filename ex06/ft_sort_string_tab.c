621b0994d54318752c47c790e53b17b271278b5d
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-24 10:50:21 +0200

    Add ex06

---
+++ ex06/ft_sort_string_tab.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/24 10:34:07 by alabreui          #+#    #+#             */
+/*   Updated: 2019/07/24 10:48:52 by alabreui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	while (tab[i] != 0)
+	{
+		j = i + 1;
+		while (tab[j] != 0)
+		{
+			if (ft_strcmp(tab[i], tab[j]) > 0)
+			{
+				temp = tab[j];
+				tab[j] = tab[i];
+				tab[i] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
